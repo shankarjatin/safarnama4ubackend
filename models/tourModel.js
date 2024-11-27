98d76590eb48bf761952ac1d3096cd13fc8f0c4a
shankarjatin <shankarjatin1005@gmail.com>
2024-11-27 15:38:31 +0530

    set up model

---
+++ models/tourModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
+  name: { type: String, required: true },
+  category: { type: String },
+  details: { type: String },
+  duration: { type: Number },
+  days: { type: String },
+  price: { type: Number },
+  image: { type: String },
+  usp: { type: String },
+  whyToChoose: { type: String },
+  content: { type: String },
+  additionalImages: [String],
+  pdfPath: { type: String }, // Path to the uploaded PDF file
+});
+
+module.exports = mongoose.model('Tour', tourSchema);
---
+++ utils/constant.js
@@ -0,0 +1,2 @@
+const API_BASE_URL =  'http://localhost:3000';
+// const API_BASE_URL =  'https://interneex.com/';
