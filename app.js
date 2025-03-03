e1fed206c766508d3f09322f63fc06b9a503ac89
shankarjatin <shankarjatin1005@gmail.com>
2025-03-03 18:07:01 +0530

    url

--- app.js
+++ app.js
@@ -27,7 +27,22 @@ app.use('/static', express.static(path.join(__dirname, 'uploads')));// Serve sta
 app.use('/api', tourRoutes);
 app.use('/api', cutomer)
 app.use('/api', order) // Mount routes
+app.get('/', (req, res) => {
+  res.send('Server is running and active!');
+  console.log('Server is running and active!');
+});
 
+// Schedule a cron job to run every 5 minutes to keep the server alive
+cron.schedule('**/2 * * * * *', async () => {
+  try {
+    console.log('Pinging server to keep it awake...');
+    // Change this to your actual server's public URL
+    await axios.get('https://safarnama4ubackend.onrender.com');
+    console.log('Server pinged successfully');
+  } catch (error) {
+    console.error('Error pinging the server:', error.message);
+  }
+});
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
